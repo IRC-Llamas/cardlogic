7304ec119e52847a1983a3fa5aa28c7f726c22f1
wodencafe <wodencafe@gmail.com>
2022-07-03 12:05:29 -0500

    Fix compile error

--- com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/evaluators/PairEvaluator.java
+++ com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/evaluators/PairEvaluator.java
@@ -1,15 +1,16 @@
 package com.foss.llamas.poker.domain.evaluators;
 
+import com.foss.llamas.poker.domain.HandResultType;
 import com.foss.llamas.poker.domain.Rank;
 import com.google.common.collect.Multimap;
 
 public class PairEvaluator extends MultiplesEvaluator {
 
 	public PairEvaluator(Multimap<Rank, Integer> rankValueMap) {
-		super(2, rankValueMap);
+		super(HandResultType.PAIR, 2, rankValueMap);
 	}
 
 	public PairEvaluator() {
-		super(2);
+		super(HandResultType.PAIR, 2);
 	}
 }
