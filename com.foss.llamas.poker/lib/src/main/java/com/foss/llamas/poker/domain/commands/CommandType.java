eb63ba964e9b58240794bc0b161a8de7be566c91
wodencafe <wodencafe@gmail.com>
2022-07-03 14:57:29 -0500

    SHOW and MUCK

--- com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/commands/CommandType.java
+++ com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/commands/CommandType.java
@@ -12,6 +12,26 @@ public enum CommandType {
 	BET,
 	RAISE,
 	FOLD,
+	/* SHOW can be used at any time,
+	 * but usually it is only performed
+	 * in the last round. This means the
+	 * player is revealing their cards.
+	 * 
+	 * A winner can only be decided if at least
+	 * one player shows their cards. 
+	 * Of all the players who show their cards,
+	 * the best hand wins.
+	 */
+	SHOW,
+	/*
+	 * MUCK can only be used in the showdown
+	 * round, it is the opposite of SHOW
+	 * and a variation of FOLD.
+	 * The player gives up without showing
+	 * their cards, acknowledging defeat
+	 * by the hand(s) that are shown.
+	 */
+	MUCk,
 	
 	// External Commands
 	CHECKMONEY,
