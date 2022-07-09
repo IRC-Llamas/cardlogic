e47bd5bc9f4ce04cc3a9ba188e3a3858f672812d
Anthony Atkinson <anthony.atkinson@grin.co>
2022-07-09 15:06:13 -0400

    StandardCardTest

---
+++ com.foss.llamas.poker/lib/src/test/java/com/foss/llamas/poker/domain/StandardCardTest.java
@@ -0,0 +1,36 @@
+package com.foss.llamas.poker.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class StandardCardTest {
+
+	@Test
+	void build_ColorRankSuit() {
+		Card card = StandardCard.build(Color.RED, Rank.FOUR, Suit.CLUBS);
+		Assertions.assertFalse(card.isWild());
+		Assertions.assertEquals(Color.RED, card.getColor());
+		Assertions.assertEquals(Rank.FOUR, card.getRank());
+		Assertions.assertEquals(Suit.CLUBS, card.getSuit());
+
+		card = StandardCard.build(Color.ANY, Rank.ANY,Suit.ANY);
+		Assertions.assertTrue(card.isWild());
+	}
+
+	@Test
+	void build_RankSuit() {
+		Card card = StandardCard.build(Rank.FOUR, Suit.CLUBS);
+		Assertions.assertEquals(Color.BLACK, card.getColor());
+		Assertions.assertEquals(Rank.FOUR, card.getRank());
+		Assertions.assertEquals(Suit.CLUBS, card.getSuit());
+	}
+
+	@Test
+	void buildJoker() {
+		Card card = StandardCard.buildJoker();
+		Assertions.assertTrue(card.isWild());
+		Assertions.assertEquals(Color.JOKER, card.getColor());
+		Assertions.assertEquals(Rank.JOKER, card.getRank());
+		Assertions.assertEquals(Suit.JOKER, card.getSuit());
+	}
+}
