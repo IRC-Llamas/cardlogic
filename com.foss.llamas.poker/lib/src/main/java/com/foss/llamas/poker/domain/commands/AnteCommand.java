1c704585ee254c2c94f67ec16768b9dd3ee61218
wodencafe <wodencafe@gmail.com>
2022-07-10 07:13:04 -0500

    Add 'required' flag for AnteCommand

--- com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/commands/AnteCommand.java
+++ com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/commands/AnteCommand.java
@@ -1,17 +1,25 @@
 package com.foss.llamas.poker.domain.commands;
 
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.beust.jcommander.ParametersDelegate;
 
 @Parameters(commandNames = AnteCommand.COMMAND_NAME, separators = "=")
 public class AnteCommand {
 	public static final String COMMAND_NAME = "ante";
+	
+	@Parameter(names = "--required", description = "Is the ante required to view the hole cards?")
+	private boolean required = false;
 
 	@ParametersDelegate
 	private CommandDelegate delegate = new CommandDelegate();
 
 	public CommandDelegate getDelegate() {
 		return delegate;
+	}
+	
+	public final boolean isRequired() {
+		return required;
 	}
 	
 }
