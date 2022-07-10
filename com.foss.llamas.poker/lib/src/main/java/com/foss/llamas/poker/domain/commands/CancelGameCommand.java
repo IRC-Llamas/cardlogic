ad5852daf08448ae1ac1ab6cf4251d3c36a78949
wodencafe <wodencafe@gmail.com>
2022-07-10 09:07:03 -0500

    Update CancelGameCommand

--- com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/commands/CancelGameCommand.java
+++ com.foss.llamas.poker/lib/src/main/java/com/foss/llamas/poker/domain/commands/CancelGameCommand.java
@@ -3,9 +3,15 @@ package com.foss.llamas.poker.domain.commands;
 import com.beust.jcommander.Parameters;
 import com.beust.jcommander.ParametersDelegate;
 
-@Parameters(commandNames = {"cancelgame"}, separators = "=")
+@Parameters(commandNames = CancelGameCommand.COMMAND_NAME, separators = "=")
 public class CancelGameCommand {
+	
+	public static final String COMMAND_NAME = "cancelgame";
 
 	@ParametersDelegate
 	private CommandDelegate delegate = new CommandDelegate();
+
+	public final CommandDelegate getDelegate() {
+		return delegate;
+	}
 }
