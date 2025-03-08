ea214d6ff39f81e409518b652da020dcd54b8f7c
wodencafe <wodencafe@gmail.com>
2025-03-08 16:00:52 -0600

    Begin implementation of persistence (#46)
    * Begin implementation of persistence
    
    * Minor tweaks
    
    * refactor: Change PersonDao to PlayerDao

---
+++ chat.llamas.cardlogic.game.api/src/main/java/chat/llamas/cardlogic/game/api/PlayerDao.java
@@ -0,0 +1,28 @@
+package chat.llamas.cardlogic.game.api;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.math.BigInteger;
+
+import chat.llamas.cardlogic.domain.game.PlayerInterface;
+public class PlayerDao {
+	   @PersistenceContext
+	   private EntityManager em;
+
+	   public PlayerInterface read(BigInteger id) {
+	      return em.find(PlayerInterface.class, id);
+	   }
+	   
+	   public void create(PlayerInterface player) {
+		   
+	   }
+	   
+	   public void update(PlayerInterface player) {
+		   
+	   }
+	   
+	   public void delete(PlayerInterface player) {
+		   
+	   }
+}
